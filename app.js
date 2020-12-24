86b552e00e5d73408ae23e83b52f2d7f85bab4d8
hari1005 <62102842+hari1005@users.noreply.github.com>
2020-12-24 19:37:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ScanScreen from "./screens/ScanScreen"
+import {createAppContainer,createSwitchNavigator} from "react-navigation"
+
+export default class App extends React.Component{
+  render(){
+      return(
+          <View>
+              
+          </View>
+      )
+  }
+}

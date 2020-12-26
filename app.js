1d1b31048dba301a9bce90a32321c350416a8878
hari1005 <62102842+hari1005@users.noreply.github.com>
2020-12-26 15:48:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ import {SigninScreen} from './screens/signinscreen'
 import {HomeScreen} from './screens/Homescreen'
 import {ExchangeScreen} from './screens/Exchangecreen'
 import {SettingsScreen} from './screens/SettingsScreen'
+import {MyBarterScreen} from './screens/MyBarterScreen'
+import {UserDetailScreen} from './screens/UserDetailScreen'
+import {NotificationScreen} from './screens/NotificationScreen'
+import {RecieverDetailScreen} from './screens/RecieverDetailScreen'
 
 const Tab=createBottomTabNavigator()
 
@@ -20,6 +24,10 @@ export default class App extends React.Component{
             <Tab.Sreens name='home' Component={HomeScreen}/>
             <Tab.Sreens name='exchange' Component={ExchangeScreen}/>
             <Tab.Sreens name='settings' Component={SettingsScreen}/>
+            <Tab.Sreens name='MyBarters' Component={MyBarterScreen}/>
+            <Tab.Sreens name='UserDetail' Component={UserDetailScreen}/>
+            <Tab.Sreens name='ReacieverDetail' Component={RecieverDetailScreen}/>
+            <Tab.Sreens name='Notifiactions' Component={NotificationScreen}/>
             </Tab.navigator> 
           </View>
       )

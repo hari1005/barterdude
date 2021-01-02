cd3be1d615b513d7ee6f45e06e6b19d43c9b4d2d
hari1005 <62102842+hari1005@users.noreply.github.com>
2021-01-02 12:04:32 +0530

    Add files via upload

---
+++ screens/ExchangeScreen.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View,TextInput,Alert } from 'react-native';
+import ScanScreen from "./screens/ScanScreen"
+import {createAppContainer,createSwitchNavigator} from "react-navigation"
+import { Button } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack'
+
+import db from '../config'
+
+function createUniqueId(){
+    this.state.itemname=random(0,99999)
+}
+
+function additem(){
+   db.collection('item').add({
+       itemname:this.state.item,
+       description:this.state.description,
+       ammount:this.state.money_worth
+   })
+}
+
+export default class ExchangeScreen extends React.Component{
+    render(){
+        this.state={
+            itemname:'',
+            description:'',
+            money_worth:''
+        }
+        return(
+            <View>
+                <TextInput onChangeText={text=>{this.setState(itemname=text)}}/>
+                <TextInput multiline={true} onChangeText={text=>{this.setState(description=text)}}/>
+                <TextInput onChangeText={text=>{this.setState(money_worth=text)}}/>
+                <Button title='additem' onPress={{additem}}/>
+                <Button title='change name' onPress={{createUniqueId}}/>
+            </View>
+        )
+    }
+}
